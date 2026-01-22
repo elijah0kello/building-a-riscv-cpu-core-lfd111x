3e56abc585f067af53b2cc7588290dd8558018e3
Elijah Okello <elijahokello90@gmail.com>
2026-01-22 12:35:06 +0200

    Update counter.v

--- counter/counter.v
+++ counter/counter.v
@@ -1,1 +1,26 @@
 
+\m5_TLV_version 1d: tl-x.org
+\m5
+   
+   // ============================================
+   // Welcome, new visitors! Try the "Learn" menu.
+   // ============================================
+   
+   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\SV
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+   m4_include_lib(['https://raw.githubusercontent.com/stevehoover/LF-Building-a-RISC-V-CPU-Core/main/lib/calc_viz.tlv']).
+	/* verilator lint_on WIDTH */
+\TLV
+   
+   $reset = *reset;
+   $cnt[15:0] = $reset ? 16'b0 : >>1$cnt + 1;
+   
+   // Assert these to end simulation (before the cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+   m4+calc_viz()
+\SV
+   endmodule
